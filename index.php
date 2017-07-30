b2cc3c4e2678277cc733242d6147822fd60e27ab
shadow <thelordsnake@hotmail.com>
2017-07-30 14:13:58 -0500

    primero

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/estilos.css">
+
+  <title>OSTEO SIN MAX</title>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-inverse  navbar-static-top" style="background-color: #033A90; " role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navegacion-fm">
+            <span class="sr-only">Desplegar / Ocultar Menu</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="#" class="navbar-brand">OSTEO SIN MAX</a>
+        </div>
+        <!-- Inicia Menu -->
+        <div class="collapse navbar-collapse" ; id="navegacion-fm">
+          <ul class="nav navbar-nav" >
+            <li class=""><a href="#">Inicio</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
+                Categorias <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Categoria #1</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Categoria #2</a></li>
+                <li><a href="#">Categoria #3</a></li>
+                <li><a href="#">Categoria #4</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contacto</a></li>
+          </ul>
+
+          <form action="" class="navbar-form navbar-right" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="buscar">
+            </div>
+            <button type="submit" class="btn btn-primary">
+              <span class="glyphicon glyphicon-search"></span>
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  </header>
+ 
+ 
+<!--aqui va el jumbotron-->  
+
+<section class="jumbotron jumbotron-fm">
+  <div class="container">
+    
+    <h1 class="ini2">OSTEO SIN MAX</h1>
+    <br>
+    <p><h3 class="ini1">es un suplemento alimenticio de origen 100% natural<h3></p>
+  </div>
+</section>
+
+
+<section class="main container"> 
+  <div class="menu_bar"> 
+
+    
+  </div>
+    
+  </section>
+    
+  </div>
+</section>
+
+<footer></footer>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
