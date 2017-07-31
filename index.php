949ebf8bb499b8451ff1e48836dd8418e1d6a53a
Miguel Angel Vázquez Vázquez <mvk_12@outlook.com>
2017-07-31 18:56:28 -0500

    Corrección de include

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <head>
 
   <meta charset="UTF-8">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/estilos.css">
+  <?php include 'includes/header_1.php'; ?>;
 
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
