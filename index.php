33bed86e4e2dee4f48d0eac43ca425017abf82fe
shadow <thelordsnake@hotmail.com>
2017-08-02 16:16:10 -0500

    miguel por que la imagen no se centra

--- index.php
+++ index.php
@@ -4,18 +4,18 @@
   <meta charset="UTF-8">
   <?php include 'includes/header_1.php';?>;
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-<title>OSTEO SIN MAX</title>
-<header>
-<nav class="navbar navbar-inverse  navbar-fixed-top" style="background-color: #033A90;  role="navigation">
-      <div class="container">
+  <title>OSTEO SIN MAX</title>
+    <header>
+      <nav class="navbar navbar-inverse  navbar-fixed-top" style="background-color: #033A90;  role="navigation">
+        <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navegacion-fm">
-            <span class="sr-only">Desplegar / Ocultar Menu</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a href="#" class="navbar-brand">OSTEO SIN MAX</a>
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navegacion-fm">
+          <span class="sr-only">Desplegar / Ocultar Menu</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="#" class="navbar-brand">OSTEO SIN MAX</a>
         </div>
         <!-- Inicia Menu -->
         <div class="collapse navbar-collapse navbar-right " ; id="navegacion-fm">
@@ -27,7 +27,7 @@
           </ul>
         </div>
       </div>
-    </nav>
+</nav>
 </header>
 </head>
 
@@ -44,64 +44,35 @@
 
 
 
-<nav class="navbar navbar-light " style="background-color: #1913C1; ">
+
 <section class="main container">
+ <article class="post clearfix">
   <div class="row">
-    <section class="post col-lg-12">
-      <ul class="nav navbar-nav">
-      <li class="#"><a href="#">Home</a></li>
-         <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">GRUPO PYR-TEC <span class="caret"></span></a>
-      <ul class="dropdown-menu">
-          <li><a href="#">Osteo sin max(tab)</a></li>
-          <li><a href="#">Osteo sin max(Crema)</a></li>
-          <li><a href="#">Page 1-3</a></li>
-        </ul>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">PALTI<span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Page 1-1</a></li>
-          <li><a href="#">Page 1-2</a></li>
-          <li><a href="#">Page 1-3</a></li>
-        </ul>
-         <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">BOTICA IZOMAL<span class="caret"></span></a>
-      <ul class="dropdown-menu">
-          <li><a href="#">Page 1-1</a></li>
-          <li><a href="#">Page 1-2</a></li>
-          <li><a href="#">Page 1-3</a></li>
-        </ul>
-          <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">NATURAL NEEM<span class="caret"></span></a>
-      <ul class="dropdown-menu">
-          <li><a href="#">Page 1-1</a></li>
-          <li><a href="#">Page 1-2</a></li>
-          <li><a href="#">Page 1-3</a></li>
-        </ul>
-    </ul>
-  </div>
-  </nav>
-
-
-  <article class="post clearfix">
-
-
-  </article>
-    <a href="#" class="thum pull-left">
-      <img src="img/oste.jpg" alt="">
+    <section class="post col-md-12">
+    <a href="#" class="thumb pull-right">
+      <img src="img/oste.jpg" alt=""  width="260" height="245">
+      <img src="img/logo.jpg" alt=""  width="260" height="245">
+      <img src="img/OSTEO_SIN_MAX.jpg" alt=""  width="260" height="245">
     </a>
-    <h2 class="post-title">
-      <a href="#"> OSTEO SIN MAX
-
-      </a>
-    </h2>
-
-
     </section>
-  </div>
-
+     <h2 class="post-title">
+      <a href="#"> OSTEO SIN MAX
+      </a>
+      <P><span class="post-fecha">26 de enero de 2015</span> por <span class="post-autor"><a href="#"> carlos arturo</a></span></P>
+      <p class="post-contenido text-justify">OSTEO SIN MAX TABLETAS CAMBIO DE IMAGEN, DEBIDO A LAS SEMEJANZAS DE LA PIRATERÍA CON EL NUESTRO, YA QUE EL PRODUCTO PIRATA ESTA AFECTANDO A MUCHAS PERSONAS.</p>
+    </h2>
+  </div>  
+</article>
 </section>
+
+
+
+
+
+
+
 <footer> copyright</footer>
-
-
-
-  <?php include 'includes/footer_1.php';?>;
+<?php include 'includes/footer_1.php';?>;
 </body>
 </html>
 
