4f97e39df80b5e5f7bc1107c841f163768cd328c
shadow <thelordsnake@hotmail.com>
2017-08-03 20:36:52 -0500

    includes cabecera

--- includes/header_2.php
+++ includes/header_2.php
@@ -1,24 +1,86 @@
-<header>   
-<nav class="navbar navbar-inverse  navbar-static-top" style="background-color: #033A90;  role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navegacion-fm">
-            <span class="sr-only">Desplegar / Ocultar Menu</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a href="#" class="navbar-brand">OSTEO SIN MAX</a>
-        </div>
+<header>
+  <nav class="navbar navbar-inverse  navbar-fixed-top" style="background-color: #033A90;"  role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navegacion-fm">
+          <span class="sr-only">Desplegar / Ocultar Menu</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="#" class="navbar-brand">OSTEO SIN MAX</a>
+      </div>
         <!-- Inicia Menu -->
-        <div class="collapse navbar-collapse navbar-right" ; id="navegacion-fm">
+        <div class="collapse navbar-collapse navbar-right " ; id="navegacion-fm">
           <ul class="nav navbar-nav" >
             <li class="#"><a href="#">Inicio</a></li>
             <li><a href="#">Quienes somos</a></li>
             <li><a href="#">Contacto</a></li>
             <li><a href="#">Aserca de</a></li>
           </ul>
-        </div>
       </div>
-    </nav>
-    </header>
+    </div>
+  </nav>
+</header>
+
+
+
+<!--aqui va el jumbotron-->
+<section class="jumbotron jumbotron-fm">
+  <div class="container">
+    <h1 class="ini2">OSTEO SIN MAX</h1>
+    <br>
+    <p><h3 class="ini1">es un suplemento alimenticio de origen 100% natural<h3></p>
+  </div>
+</section>
+<section>
+<div class="container">
+  <div class="row">
+    <div class="col-xs-3">
+    </div>
+    <div class="col-xs-9">
+           <ul id="main-menu" class="nav nav-pills">
+        <li class="active"><a href="index.php">inicio</a></li>
+        <li class="dropdown">
+          <a href="dropdown-toggle" data-toggle="dropdown" href="#">grupo pyr-tec<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="osteo_sin_max.php">osteo sin max (tab)</a></li>
+            <li><a href="osteo_sin_max.php">osteo sin max (Crema)</a></li>
+            <li><a href="osteo_sin_max.php">opticol(tab)</a></li>
+            <li><a href="osteo_sin_max.php">osteo sin max (tab)</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="dropdown-toggle" data-toggle="dropdown" href="#">grupo pyr-tec<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="osteo_sin_max.php">osteo sin max (tab)</a></li>
+            <li><a href="osteo_sin_max.php">osteo sin max (Crema)</a></li>
+            <li><a href="osteo_sin_max.php">opticol(tab)</a></li>
+            <li><a href="osteo_sin_max.php">osteo sin max (tab)</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="dropdown-toggle" data-toggle="dropdown" href="#">grupo pyr-tec<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="osteo_sin_max.php">osteo sin max (tab)</a></li>
+            <li><a href="osteo_sin_max.php">osteo sin max (Crema)</a></li>
+            <li><a href="osteo_sin_max.php">opticol(tab)</a></li>
+            <li><a href="osteo_sin_max.php">osteo sin max (tab)</a></li>
+          </ul>
+        </li>
+                <li class="dropdown">
+          <a href="dropdown-toggle" data-toggle="dropdown" href="#">grupo pyr-tec<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="osteo_sin_max.php">osteo sin max (tab)</a></li>
+            <li><a href="osteo_sin_max.php">osteo sin max (Crema)</a></li>
+            <li><a href="osteo_sin_max.php">opticol(tab)</a></li>
+            <li><a href="osteo_sin_max.php">osteo sin max (tab)</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  
+</div>
+</section>
