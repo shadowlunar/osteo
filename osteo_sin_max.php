8564ae381c6ef54c44f6979ee1fff366d2bc37e4
shadow <thelordsnake@hotmail.com>
2017-08-03 20:50:50 -0500

    segunda pagina

--- osteo_sin_max.php
+++ osteo_sin_max.php
@@ -1,1 +1,14 @@
-osteo_sin_max
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <?php include 'includes/header_1.php';?>
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <title>OSTEO SIN MAX</title>
+</head>
+<body>
+<?php include 'includes/header_2.php';?>
+
+
+</body>
+</html>
