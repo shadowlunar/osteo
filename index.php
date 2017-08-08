f0029a7998a7ed1d666a23a004a1656d16bcc297
shadow <thelordsnake@hotmail.com>
2017-08-07 22:13:49 -0500

    cambios esactos

--- index.php
+++ index.php
@@ -23,15 +23,15 @@
     <!-- Wrapper for slides -->
     <div class="carousel-inner">
       <div class="item active">
-        <img src="img/HEPATOGENOL_PLUS_MAX.jpg" alt="Los Angeles" >
+        <img src="img/HEPATOGENOL_PLUS_MAX.jpg" alt="Los Angeles" style="width:100%";>
       </div>
 
       <div class="item">
-        <img src="img/oste.jpg" alt="Chicago" >
+        <img src="img/oste.jpg" alt="Chicago" style="width:100%";>
       </div>
     
       <div class="item">
-        <img src="img/OSTEO_SIN_MAX.jpg" alt="New york" >
+        <img src="img/OSTEO_SIN_MAX.jpg" alt="New york" style="width:100%"; >
       </div>
     </div>
 
