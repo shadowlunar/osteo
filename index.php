b979183154518fd86128b49ec9231117e0cc51ba
shadow <thelordsnake@hotmail.com>
2017-08-08 17:51:21 -0500

    index 70 porciento revisar si hay alguna falla

--- index.php
+++ index.php
@@ -10,8 +10,7 @@
 <?php include 'includes/header_2.php';?>
 
 
-<div class="container">
-  <h2>Carousel Example</h2>  
+<div class="container">  
   <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
     <ol class="carousel-indicators">
@@ -46,6 +45,28 @@
     </a>
   </div>
 </div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-xs-8">
+      <section id="intro">
+        <h3>HIERBA DE SAN JUAN</h3>
+        <p>En uso interno está indicada en el tratamiento de la ansiedad, depresión, terrores nocturnos, transtornos neurovegetativos asociados al climaterio, enuresis; disquinesias biliares, espasmos gastrointestinales, gastritis, ulcus gastroduodenales, diarreas, colon irritable; asma, varices, hemorroides, fragilidad capilar.</p>
+
+        <p>La hipericina produce un efecto antidepresivo, al inhibir la acción de la monoamino oxidasa (IMAO), la hiperforina es un tranquilizante suave. El aceite esencial y los taninos le confieren una acción antiséptica, astringente y cicatrizante (vulneraria); los flavonoides, un efecto colagogo, espasmolítico y vitamínico P (venotónico, vasoprotector capilar).</p>
+
+        <p>Antidepresivo: Se ha hablado mucho de este tema, en donde se ha demostrado que la presencia de hipericina le otorga propiedades antidepresivas en los casos de depresión leve. Esta substancia actúa en el organismo de manera similar a como lo hacen las drogas de síntesis sin presentar los inconvenientes de las mismas. Sin embargo, algunos estudios parecen orientarse últimamente hacia la hiperforina como principal componente determinante en esta propiedad. No tiene ningún efecto en los casos de depresión aguda.</p>
+        <p>Tónico nervioso: Su uso prolongado -entre unos 4 y 6 meses- constituye un tónico reparador del sistema nervioso. A diferencia de otros sedativos, no tiene efectos negativos por acumulación de toxinas en la sangre, sobre todo para el hígado.</p>
+
+        <p>Digestivo: Buen remedio cuando aparece acidez gástrica y, sobre todo, en el tratamiento de la úlcera de estómago, diarreas y vómitos.</p>
+      </section>
+    </div>
+    <div class="col-xs-4 im "></div>
+    <img src="img/oste.jpg" alt="medicamento" width="342" height="442">
+  </div>
+</div>
+
+
 
 
 <?php include 'includes/footer_2.php';?>
